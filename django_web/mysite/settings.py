034fe7231a35d6162dc843ded694a39432cffcd1
Koi <bealiruao@gmail.com>
2025-08-25 11:28:25 +0700

    fixes to urls users app name

--- django_web/mysite/settings.py
+++ django_web/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
-    'accounts.apps.AccountsConfig',
+    'users.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
