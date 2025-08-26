58e652bebc0bd6fc8080f325252666523c26aa9d
Koi <bealiruao@gmail.com>
2025-08-26 16:19:11 +0700

    added function login and register

--- django_web/users/views.py
+++ django_web/users/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from .models import Reader
 # Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 
 def login_view(request):
-    return render(request, 'users/login.html')  # Note the users/ prefix
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            print("b")
+            return redirect('core:dashboard')
+    else:
+        form = AuthenticationForm()  
+    return render(request, 'users/login.html', {'form': form})  # Note the users/ prefix
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('users:login')
+            
+            new_user = form.save()
+            login(request, form.save())
+            email = request.POST.get('email')
+            
+            Reader.objects.create(
+                user = new_user,
+                email = email,
+                password = new_user.password
+            )
+            print("New user register successfully")
+            
+            return redirect('core:dashboard')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})  # Note the users/ prefix
