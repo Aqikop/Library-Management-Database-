b257feefe02b556a25a14685a3fb6c25bc5c166c
Koi <bealiruao@gmail.com>
2025-08-26 16:18:48 +0700

    add model Reader

--- django_web/users/models.py
+++ django_web/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Reader(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    password = models.CharField(max_length=128)
+    # firstname = models.CharField(max_length=100)
+    # lastname = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    phone_no = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user} Profile"
+    
+# create user for username, pass, email first, add them to reader field
+# other can be create s
+# pass yourpassword123
