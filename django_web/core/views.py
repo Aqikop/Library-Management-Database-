04cccfbfd20e1da3afbb37d6169b792c22beea1b
Koi <bealiruao@gmail.com>
2025-08-29 18:52:46 +0700

    added differences view base on user authentication

--- django_web/core/views.py
+++ django_web/core/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'core/homepage.html')  # Note the core/ prefix
+    context = {
+        'is_authenticated': request.user.is_authenticated
+    }
+    return render(request, 'core/homepage.html', context)  # Note the core/ prefix
 
 def dashboard_view(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')  # Use namespaced URL name
-    return render(request, 'core/dashboard.html')  # Note the core/ prefix
+    context = {
+        'is_authenticated': request.user.is_authenticated
+    }
+    return render(request, 'core/dashboard.html', context)
